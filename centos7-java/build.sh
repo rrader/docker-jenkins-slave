342cddb887b97c5b8ce98f82b6ddef0c5dc7370a
Roman Rader <antigluk@gmail.com>
2015-03-21 18:20:00 +0200

    Don't copy keys in building directory

--- centos7-java/build.sh
+++ centos7-java/build.sh
@@ -2,11 +2,4 @@
 
 source ../local_settings.sh
 
-mkdir -p ./resources
-cp -R ../keys/* ./resources
-
 $docker build -t antigluk/jenkins-slave-centos7-java .
-
-
-#cd ../
-#bash build.sh centos6 centos6.4
