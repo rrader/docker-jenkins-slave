e0949de7bcc94423e1c7641033a533b769560542
Roman Rader <roman.rader@gmail.com>
2015-03-24 15:25:57 +0200

    No caching of .m2 directory: simultaneous jobs don't work

--- slaves/centos6-java/add_slave.sh
+++ slaves/centos6-java/add_slave.sh
@@ -2,4 +2,4 @@
 
 source ../local_settings.sh
 
-$docker run -d --name="$1" -v /tmp/docker-m2cache:/root/.m2:rw antigluk/jenkins-slave-centos6-java -labels docker-centos6-java -name "$1"
+$docker run -d --name="$1" antigluk/jenkins-slave-centos6-java -labels docker-centos6-java -name "$1"
--- slaves/centos7-java/add_slave.sh
+++ slaves/centos7-java/add_slave.sh
@@ -2,4 +2,4 @@
 
 source ../local_settings.sh
 
-$docker run -d --name="$1" -v /tmp/docker-m2cache:/root/.m2:rw antigluk/jenkins-slave-centos7-java -labels docker-centos7-java -name "$1"
+$docker run -d --name="$1" antigluk/jenkins-slave-centos7-java -labels docker-centos7-java -name "$1"
